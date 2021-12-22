977d261c191e030d69dba70ea6737790b474e005
AFOEK <felix.montalfu@gmail.com>
2021-12-22 11:17:37 +0700

    adding file structure

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_socketio import *
 import hashlib
 
 app = Flask(__name__)
-hash_obj = hashlib.sha512(b'0x9f17cd320d8d4abd9dc18535391dd0b09784dcd9590d9fc11764331eae69f0b1389d3582ab69ef71fa6605dc470b5f3737f581c2807f9f')
+hash_obj = hashlib.sha512(b'0x9f17cd320d8d4abd9dc18535391dd0b09784dcd9590d9fc11764331eae69f0b1389d3582ab69ef71fa6605dc470b5f3737f581c2807f9f')  #hexa from 60 digit integer
 hash_dig = hash_obj.hexdigest()
 app.config['SECRET_KEY'] = hash_dig
 socketio = SocketIO(app)
@@ -16,8 +16,11 @@ def login():
 def session():
     return render_template('index.html')
 
-def messageReceived(methods=['GET', 'POST']):
+def mesgReceived(methods=['GET', 'POST']):
     return 200, 'msg already received'
 
+@socketio.on('event')
+def event_handler(json, methods=['GET', 'POST']):
+    socketio.emit('response_return', json, callback=msgReceived)
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=80)
