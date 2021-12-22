b4e55c045778a32f1cb9e65179d91c99130244d4
AFOEK <felix.montalfu@gmail.com>
2021-12-22 16:41:32 +0700

    Adding Login form structure

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import hashlib
 
 app = Flask(__name__)
 hash_obj = hashlib.sha512(b'0x9f17cd320d8d4abd9dc18535391dd0b09784dcd9590d9fc11764331eae69f0b1389d3582ab69ef71fa6605dc470b5f3737f581c2807f9f')  #hexa from 60 digit integer
-hash_dig = hash_obj.hexdigest()
-app.config['SECRET_KEY'] = hash_dig
+hash_dig = hash_obj.hexdigest() #make hash object become hexa
+app.config['SECRET_KEY'] = hash_dig #adding secret key to apps
 socketio = SocketIO(app)
 
 # @app.route('/')
