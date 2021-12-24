711139c406d7bc7f1b43dbfc4a52a149a6a2800f
felixbm <felix.bmmm@gmail.com>
2021-12-24 20:35:03 +0700

    feat: added swagger ui for api docs (for now the docs is a dummy one)

--- main.py
+++ main.py
@@ -3,29 +3,47 @@ from flask_socketio import *
 import hashlib
 
 app = Flask(__name__)
-hash_obj = hashlib.sha512(b'0x9f17cd320d8d4abd9dc18535391dd0b09784dcd9590d9fc11764331eae69f0b1389d3582ab69ef71fa6605dc470b5f3737f581c2807f9f')  #hexa from 60 digit integer
-hash_dig = hash_obj.hexdigest() #make hash object become hexa
-app.config['SECRET_KEY'] = hash_dig #adding secret key to apps
+
+# hexa from 60 digit integer
+hash_obj = hashlib.sha512(
+    b'0x9f17cd320d8d4abd9dc18535391dd0b09784dcd9590d9fc11764331eae69f0b1389d3582ab69ef71fa6605dc470b5f3737f581c2807f9f')
+hash_dig = hash_obj.hexdigest()  # make hash object become hexa
+app.config['SECRET_KEY'] = hash_dig  # adding secret key to apps
 socketio = SocketIO(app)
 
 # @app.route('/')
 # def login():
 #     return render_template('login_frm.html')
 
+
 @app.route('/')
 def soon():
     return render_template('soon.html')
+
+
+@app.route('/docs')
+def swagger_ui():
+    return render_template('swagger_ui.html')
+
+
+@app.route('/spec')
+def get_spec():
+    return send_from_directory(app.root_path, 'api-docs.yaml')
+
 
 @app.route('/main')
 def session():
     return render_template('index.html')
 
+
 def mesgReceived(methods=['GET', 'POST']):
     return 200, 'msg already received'
+
 
 @socketio.on('evt')
 def event_handler(json, methods=['GET', 'POST']):
     socketio.emit('response_return', json, callback=msgReceived)
-    
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=8080)
